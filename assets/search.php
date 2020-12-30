26ebe2e9d7fc730a516220be4a21e150d452bfb3
Muhammad Ridha <muhammadridha11@gmail.com>
2020-12-30 14:30:27 +0700

    Membuat halaman search

---
+++ assets/search.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Simple Search Engine Demo</title>
+</head>
+<body>
+
+    <!-- Begin Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-2 py-3">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/img/search-icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                USearch
+            </a>
+            <form class="form-inline my-2 my-lg-0" id="form-seacrh-nav">
+                <input class="form-control mr-sm-2" type="search" placeholder="Enter keyword" aria-label="Search">
+              <button class="btn bg-blue my-2 my-sm-0" type="submit">Search</button>
+          </form>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto"></ul>
+        <a href="#"><img src="assets/img/github-icon.png" width="30" height="30" class="d-inline-block align-top"></a>
+    </div>
+</div>
+</nav>
+<!-- End Navbar -->
+
+
+<div class="container mt-5">
+    <div class="row align-items-bottom">
+        <div class="col-8">
+
+        </div>
+    </div>
+</div> 
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
